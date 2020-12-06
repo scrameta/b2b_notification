40426f17e6588f0dfa010e72631e540f346431f4
scrameta <scrameta@gmail.com>
2020-12-06 18:10:59 +0100

    Remove key

--- setup/mktdata/fetch.pl
+++ setup/mktdata/fetch.pl
@@ -8,6 +8,6 @@ while (<>)
 	chomp;
 	my $name = $_;
 	print "Fetching $name\n";
-	`wget  "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${name}&apikey=***REMOVED***&datatype=csv&outputsize=compact" -O ${name}_20201203.csv`;
+	`wget  "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${name}&apikey=REPLACEME&datatype=csv&outputsize=compact" -O ${name}_20201203.csv`;
 	sleep 10;
 }
